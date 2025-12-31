80a3cbd52980252a8b9276cc33e79e5de9a8d591
Finsa-SC <finsakusumaputra@gmail.com>
2025-12-31 11:04:03 +0700

    add new icon, and add ApplicationListFragment.kt Final code

--- app/src/main/java/com/example/gawe17/myjob/ApplicationListFragment.kt
+++ app/src/main/java/com/example/gawe17/myjob/ApplicationListFragment.kt
@@ -5,22 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gawe17.R
+import com.example.gawe17.core.network.ApiHelper
+import com.example.gawe17.databinding.FragmentApplicationListBinding
+import com.example.gawe17.explore.JobAdapter
+import com.example.gawe17.model.JobCardMode
+import com.example.gawe17.model.JobList
+import org.json.JSONObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ApplicationListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ApplicationListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var rv: RecyclerView
+    private lateinit var adapter: JobAdapter
+
+    val jobList = mutableListOf<JobList>()
+
+    var _binding: FragmentApplicationListBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,21 +43,12 @@ class ApplicationListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_application_list, container, false)
+    ): View {
+        _binding = FragmentApplicationListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ApplicationListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ApplicationListFragment().apply {
@@ -56,5 +57,83 @@ class ApplicationListFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rv = binding.rvJob
+
+        adapter = JobAdapter(
+            jobs = jobList,
+            mode = JobCardMode.Applied,
+            onApply = {},
+            onMark = {},
+            onCard = {}
+        )
+
+        rv.adapter = adapter
+        rv.layoutManager = LinearLayoutManager(requireContext())
+
+        loadJobApplication()
+    }
+
+    private fun loadJobApplication(){
+        Thread{
+            val (code, response) = ApiHelper.get("job-applications")
+            val jsonResponse = if(response!=null) JSONObject(response) else null
+            activity?.runOnUiThread {
+                if(code==200&&jsonResponse!=null){
+                    val jsonData = jsonResponse.getJSONArray("data")
+                    if(jsonData.length() <= 0){
+                        isProblem(true, "there are no jobs you applied for, let's find some", R.drawable.furina_cherfull)
+                    }else{
+                        isProblem(false)
+                        jobList.clear()
+                        for(i in 0 until jsonData.length()){
+                            val json = jsonData.getJSONObject(i)
+                            val job = json.getJSONObject("job")
+                            val company = job.getJSONObject("company")
+                            jobList.add(JobList(
+                                jobId = job.getInt("id"),
+                                jobName = job.getString("name"),
+                                jobCompanyName = company.getString("name"),
+                                jobLocationType = job.getString("locationType"),
+                                jobLocationRegion = job.getString("locationRegion"),
+                                jobExperience = job.getString("yearOfExperience"),
+                                quota = job.getInt("quota")
+                            ))
+                        }
+                    }
+
+                }else if(jsonResponse!=null){
+                    isProblem(true, jsonResponse.optString("message", "Sorry, there is a small error"), R.drawable.furina_shock)
+                }else{
+                    isProblem(true, "Server Doesn't Responding. It's Not You, It's Us", R.drawable.furina_crying)
+                }
+                adapter.notifyDataSetChanged()
+            }
+        }.start()
+    }
+
+    private fun isProblem(status: Boolean, text: String? = null, image: Int? = null){
+        val playout = view?.findViewById<View>(R.id.problemLayout)
+        val ptext = view?.findViewById<TextView>(R.id.txtProblem)
+        val pimage= view?.findViewById<ImageView>(R.id.imgProblem)
+        if(status){
+            jobList.clear()
+            playout?.visibility = View.VISIBLE
+            rv.visibility = View.GONE
+            text?.let{ptext?.text=it}
+            image?.let{pimage?.setImageResource(it)}
+        }else{
+            rv.visibility = View.VISIBLE
+            playout?.visibility = View.GONE
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadJobApplication()
     }
 }
